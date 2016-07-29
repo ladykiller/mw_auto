241f0da64af144520d1836e55291cc11ce8b4279
frainmeng <frainmeng@gmail.com>
2016-07-29 15:40:00 +0800

    解决并发问题

--- src/cn/mwee/auto/deploy/service/execute/SimpleTaskExecutor.java
+++ src/cn/mwee/auto/deploy/service/execute/SimpleTaskExecutor.java
@@ -72,7 +72,9 @@ public class SimpleTaskExecutor implements TaskExecutor {
     public void executeChain(FlowTask flowTask) {
         // TODO Auto-generated method stub
         try {
-            flowManagerService.updateTaskStatus(flowTask.getId(), TaskState.ING.name());
+            if (!flowManagerService.updateTaskStatus(flowTask.getId(), TaskState.ING.name())) {
+                return;
+            }
             if (!execute(flowTask)) {
                 throw new Exception();
             }
